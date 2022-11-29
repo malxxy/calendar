b0e3acb754697f54542b1b92e102c8f9cd5782d8
malxxy <maliayraguen@gmail.com>
2022-11-29 12:38:17 -0800

    past present future for loop

--- Develop/script.js
+++ Develop/script.js
@@ -27,6 +27,9 @@ $(document).ready(function() {
 
   setInterval(beginTime,1000);
 
+  // How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
   $(".saveBtn").click(function(){
     // localStorage
     var thing = $(this);  // capture the user input
@@ -37,7 +40,6 @@ $(document).ready(function() {
     var myHour = $(this).parent().attr('id');
     console.log(myHour); // hour that event is entered into
     let pullEvent = JSON.parse(localStorage.getItem("myEvent"));
-
     console.log(pullEvent);
 
     var temp = {
@@ -52,42 +54,40 @@ $(document).ready(function() {
   localStorage.setItem("myEvent", JSON.stringify(pullEvent));
   localStorage.setItem(myHour, JSON.stringify(myEvent));
 
-  var currentHour = dayjs().format("HH");
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
 
-  // Past, present and future
-  if (currentHour) {
-    $(".description")
-  }
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+  });
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  // Past, present and future
 
-  // make a variable using dayjs to determine 24 hour time
-  // if userInput === id-currenthour) {
-  //  code to designate as presemt
-  //} else if (userInput < id-currenthour) {
-  // code to designate as past
-  //}
-  // else {
-  // code to designate as future
-  //}
-  //};
+  var currentHour = dayjs().format("H");
+  console.log("current hour",currentHour);
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+  var allHours = $(".hours");
+  hourArray = [];
+  console.log("allHours",allHours);
+  for (let index = 0; index < allHours.length; index++) {
+    var hours = $(allHours[index]).attr("id");
+    var hourOnly = hours.replace('hour-','');
+    hourArray.push(hourOnly);
+  }
 
+  console.log("hourArray",hourArray);
+
+  for (let index = 0; index < hourArray.length; index++) {
+    if (hourArray[index] == currentHour) {
+      $(".description").css("background-color","white");
+    } else if (hourArray[index] > currentHour) {
+      $(".description").css("background-color","lavender");
+    } else {
+      $(".description").css("background-color","pink");
+    }
+  };
 });
