50ba5c9ed457b1accd08f3d449f1eb81f37be56b
malxxy <maliayraguen@gmail.com>
2022-11-29 13:27:38 -0800

    local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,66 +1,19 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// var root = $("root"); Is this what the above question refers to?
 $(document).ready(function() {
-
-  // Initalize localStorage
-  var savedData = localStorage.getItem('myEvent');
-  if(!savedData) {
-    savedData = [];
-    localStorage.setItem("myEvent", JSON.stringify(savedData))
-  }
-
-  // Clear calendar every 24 hour
-  var currentTime = dayjs().format("HH:mm:ss");
-  if (currentTime === "20:15:00") {
-   $(".description").text("");
-  };
-
-  var saveBtn = $('.btn saveBtn'); // Save button variable
-
-// display time at top of calendar
+  // display time at top of calendar
   function beginTime () {
     var currentDateTime = dayjs().format("MM/DD/YY HH:mm:ss");
     $('#currentDay').text(currentDateTime);
   };
 
   setInterval(beginTime,1000);
-
-  // How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  $(".saveBtn").click(function(){
-    // localStorage
-    var thing = $(this);  // capture the user input
-    console.log(thing.siblings(".description").val());  // Traverse the DOM
-    var myEvent = thing.siblings(".description").val();
-    // localStorage.setItem("myEvent",JSON.stringify(myEvent));  // we just overwrite the existing data
-    console.log(myEvent);
-    var myHour = $(this).parent().attr('id');
-    console.log(myHour); // hour that event is entered into
-    let pullEvent = JSON.parse(localStorage.getItem("myEvent"));
-    console.log(pullEvent);
-      // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-    var temp = {
-      hour: myHour,
-      event: myEvent
-    };
-
-  // Add our new data to our dataset
-  pullEvent.push(temp);
-  console.log(pullEvent);
-  let JSONevents = JSON.stringify(pullEvent);
-  localStorage.setItem("myEvent", JSON.stringify(pullEvent));
-  localStorage.setItem(myHour, JSON.stringify(myEvent));
-  });
+  var currentTime = dayjs().format("HH:mm:ss"); // Clear calendar every 24 hour
+  if (currentTime === "20:15:00") {
+   $(".description").text("");
+  };
 
   // Past, present, and future
   var currentHour = dayjs().format("H");
   console.log("current hour",currentHour);
-
   $(".hours").each(function() {
     var hour = $(this).attr("id");
     if (hour == currentHour) { 
@@ -71,4 +24,40 @@ $(document).ready(function() {
       $(this).css("background-color","pink"); // past
     }
   })
+  // Initalize localStorage
+  var saveBtn = $('.btn saveBtn'); // Save button variable
+
+  $(".saveBtn").click(function(){
+    // localStorage
+    // TODO: Add code to get any user input that was saved in localStorage and set
+    // the values of the corresponding textarea elements. HINT: How can the id
+    // attribute of each time-block be used to do this?
+    var myEvent = $(this).siblings(".description").val(); // Traverse the DOM
+    console.log("myEvent",myEvent);
+    localStorage.setItem("Event",JSON.stringify(myEvent)); // set Item of textarea
+
+    var myHour = $(this).parent().attr('id');
+    console.log("myHour",myHour); // hour that event is entered into
+    localStorage.setItem("myHour",JSON.stringify(myHour)); //set Item of hour
+    
+    let pullEvent = JSON.parse(localStorage.getItem("myEvent",myEvent)); // get item from storage
+    console.log(pullEvent);
+  });
 });
+
+// var savedData = localStorage.getItem('myEvent');
+// if(!savedData) {
+//     savedData = [];
+//     localStorage.setItem("myEvent", JSON.stringify(savedData))
+//   }
+// var temp = {
+//   hour: myHour,
+//   event: myEvent
+// };
+
+// // Add our new data to our dataset
+// pullEvent.push(temp);
+// console.log(pullEvent);
+// let JSONevents = JSON.stringify(pullEvent);
+// localStorage.setItem("myEvent", JSON.stringify(pullEvent));
+// localStorage.setItem(myHour, JSON.stringify(myEvent));
